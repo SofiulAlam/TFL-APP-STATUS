0a0dac60c442e625cf38198e02cb4f54f9d4f6c7
BravoTwoZero <sofiul.alam@hotmail.com>
2024-05-06 10:55:15 +0100

    Adding more test

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -2,6 +2,8 @@ import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 
 import TubeStatus from "../components/tube-status/TubeStatus";
+import statusService from "../services/status-service";
+
 describe("Renders main page correctly", async () => {
   it("Should render the page correctly", async () => {
     // Setup
@@ -10,5 +12,33 @@ describe("Renders main page correctly", async () => {
 
     // Expectations
     expect(h1).not.toBeNull();
+  });
+
+  it("renders correctly", () => {
+    render(<TubeStatus />);
+  });
+
+  it("Should render tube line details correctly", async () => {
+    // Mocking the API response
+    global.fetch = async () => ({
+      json: async () => [
+        { id: "1", name: "Bakerloo", status: "Good Service" },
+        { id: "2", name: "Central", status: "Good Service" },
+      ],
+    });
+
+    // Setup
+    render(<TubeStatus />);
+
+    // Wait for the API call to resolve
+    await screen.findAllByText("Bakerloo : Good Service");
+    await screen.findAllByText("Central : Good Service");
+
+    // Expectations
+    const bakerlooElement = screen.getAllByText(/Bakerloo : Good Service/i);
+    const centralElement = screen.getAllByText(/Central : Good Service/i);
+
+    expect(bakerlooElement).toBeTruthy();
+    expect(centralElement).toBeTruthy();
   });
 });
